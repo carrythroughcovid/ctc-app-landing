a5d0ba741e29cfa9e483f8cfd52d19e948599339
Matt Perrott <matt.perrott@rea-group.com>
2020-04-17 00:07:53 +1000

    initial update of content

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,6 +8,8 @@ import { ButtonInternalLink as Button } from '../components/shared/Button'
 import { ButtonLink } from '../components/shared/Button'
 import MissionImage from '../images/about-mission.png'
 import WhoWeAreImage from '../images/about-who-we-are.png'
+import ScreenshotBusiness from '../images/screenshot_business.png'
+import ScreenshotCommunity from '../images/screenshot_community.png'
 import MobileDown from '../images/mobile-down.png'
 import MobileUp from '../images/mobile-up.png'
 import media from '../utils/media'
@@ -15,32 +17,14 @@ import theme from '../styles/theme'
 import SEO from '../components/shared/SEO'
 
 const GenericSection = styled.div`
-  height: 400px;
   background-color: ${props => props.color};
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 2rem;
   ${media.sm`
     flex: 1;
   `}
-`
-
-const LargeSection = styled.div`
-  background-color: white;
-
-  display: none;
-  ${media.md`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    height: 400px;
-  `}
-`
-
-const LargeSectionContainer = styled.div`
-  width: 800px;
-  height: 400px;
-  position: relative;
 `
 
 const ColumnRowDiv = styled.div`
@@ -64,6 +48,10 @@ const SectionTitle = styled.div`
   margin-bottom: 1em;
 `
 
+const LargeTitle = styled.h1`
+  color: white;
+`
+
 const CenteredText = styled.div`
   text-align: center;
 `
@@ -71,16 +59,6 @@ const CenteredText = styled.div`
 const SmallImage = styled.img`
   width: 60px;
   margin-bottom: 1rem;
-`
-
-const NumberTotal = styled.div`
-  font-size: 5rem;
-  color: ${({ theme }) => theme.colour.violet};
-`
-
-const WhatsUpDetails = styled.div`
-  color: white;
-  width: 300px;
 `
 
 const SectionContainer = styled.div`
@@ -95,22 +73,12 @@ const SectionContainer = styled.div`
   `}
 `
 
-const AbsoluteImage = styled.img`
-  width: 500px;
-  position: absolute;
+const NormalImage = styled.img`
+  width: 100%;
+  padding-top: 2rem;
 `
 
-const MobileDownImage = styled(AbsoluteImage)`
-  top: 0;
-  right: 0;
-`
-
-const MobileUpImage = styled(AbsoluteImage)`
-  bottom: 0;
-  left: 0;
-`
-
-const About = ({ data }) => {
+const About = () => {
   return (
     <>
       <SEO
@@ -122,68 +90,31 @@ const About = ({ data }) => {
           <GenericSection color={theme.colour.violet}>
             <ColumnSection width="300px">
               <SmallImage src={MissionImage}></SmallImage>
-              <SectionTitle>OUR MISSION</SectionTitle>
+              <SectionTitle>CARRY FOR BUSINESSES</SectionTitle>
               <CenteredText>
-                We want to help keep small businesses afloat and enable
-                community support through technology.
+                Businesses who have been affected by shutdowns can post updates
+                to trading, so the community can stay informed.
               </CenteredText>
+              <NormalImage src={ScreenshotBusiness} />
             </ColumnSection>
           </GenericSection>
           <GenericSection color="#eef0f8">
             <ColumnSection width="300px">
               <SmallImage src={WhoWeAreImage}></SmallImage>
-              <SectionTitle>WHO WE ARE</SectionTitle>
+              <SectionTitle>CARRY FOR COMMUNITY</SectionTitle>
               <CenteredText>
-                A group of volunteers and our service is completely FREE of
-                charge.
+                Community supporters can browse through the different business,
+                and learn how they can support small businesses.
               </CenteredText>
+              <NormalImage src={ScreenshotCommunity} />
             </ColumnSection>
           </GenericSection>
         </ColumnRowDiv>
-
-        <LargeSection>
-          <LargeSectionContainer>
-            <MobileUpImage src={MobileUp}></MobileUpImage>
-            <MobileDownImage src={MobileDown}></MobileDownImage>
-          </LargeSectionContainer>
-        </LargeSection>
-
         <SectionContainer color={theme.colour.red}>
-          <ColumnSection>
-            <p style={{ color: 'white' }}>HELP EXPAND THE COMMUNITY</p>
-            <p style={{ color: 'white' }}>
-            </p>
-          </ColumnSection>
-          <ColumnSection>
-            <div style={{ color: 'white', marginBottom: '1rem' }}>
-              PROMOTE YOUR NEW SERVICES
-            </div>
-            <WhatsUpDetails>
-              {' '}
-              Get your business into the pockets of the community.
-            </WhatsUpDetails>
-          </ColumnSection>
-        </SectionContainer>
-
-        <SectionContainer>
-          <ColumnSection width="300px">
-            <div style={{ marginBottom: '2rem' }}>
-              If you are looking to inform your loyal customers or make new
-              connections then we can help promote any of your new services
-              without any fee.
-            </div>
-            <Button to="/signup">Sign up now!</Button>
-          </ColumnSection>
-          <ColumnSection width="300px">
-            <SectionTitle>HAVE A QUESTION FOR US?</SectionTitle>
-            <div style={{ marginBottom: '2rem' }}>
-              Still not sure if this is for you, or have a few questions?  Our
-              team will be happy to help!
-            </div>
-            <ButtonLink href="mailto:contact@carrythroughcovid.com">
-              Contact our team
-            </ButtonLink>
-          </ColumnSection>
+          <LargeTitle>Got questions?</LargeTitle>
+          <ButtonLink href="mailto:contact@carrythroughcovid.com">
+            Contact our support team
+          </ButtonLink>
         </SectionContainer>
       </Page>
     </>
